7fc77ed69ca1abe878e6560526813d00d8464713
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-12 13:41:31 +0100

    Feature: Lista in html e line-through

--- script/myScript.js
+++ script/myScript.js
@@ -1,3 +1,13 @@
+// MILESTONE 1
+// Stampare all’interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 const { createApp } = Vue;
 
 createApp({
@@ -5,7 +15,21 @@ createApp({
   data() {
 
     return {
-        
+      
+      todos: [
+        {
+            text: 'Fare i compiti',
+            done: false
+        },
+        {
+            text: 'Fare la spesa',
+            done: true
+        },
+        {
+            text: 'Fare il bucato',
+            done: false
+        }
+      ]
     }
 
   },
